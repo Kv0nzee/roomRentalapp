aae6591b8beeb2b1290cdba4f26602ea8d6cfe15
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2024-01-29 23:22:13 +0630

    googleoAuth

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -94,13 +94,13 @@ const LoginModal = () => {
                     outline
                     label='Continue with Google'
                     icon={FcGoogle}
-                    onClick={() => {}}
+                    onClick={() => signIn('google')}
                 />
                 <Button 
                     outline
                     label='Continue with GitHub'
                     icon={AiFillGithub}
-                    onClick={() => {}}
+                    onClick={() => signIn('github')}
                 />
                 <div className=' flex flex-row justify-center items-center text-neutral-700 text-center mt-4 font-light'>
                     <div className='flex flex-row items-center gap-2'>
